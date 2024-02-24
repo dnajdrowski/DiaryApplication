c8dea3ca15bee90453102ff1f066cd6896c14d44
Daniel Najdrowski <danielnajdrowski@gmail.com>
2024-02-24 21:28:57 +0100

    Splash screen implementation

--- app/src/main/java/pl/dnajdrowski/diaryapplication/MainActivity.kt
+++ app/src/main/java/pl/dnajdrowski/diaryapplication/MainActivity.kt
@@ -3,11 +3,13 @@ package pl.dnajdrowski.diaryapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import pl.dnajdrowski.diaryapplication.ui.theme.DiaryApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             DiaryApplicationTheme {
 
--- app/src/main/java/pl/dnajdrowski/diaryapplication/ui/theme/Theme.kt
+++ app/src/main/java/pl/dnajdrowski/diaryapplication/ui/theme/Theme.kt
@@ -73,7 +73,7 @@ private val DarkColors = darkColorScheme(
 )
 
 @Composable
-fun AppTheme(
+fun DiaryApplicationTheme(
   useDarkTheme: Boolean = isSystemInDarkTheme(),
   content: @Composable() () -> Unit
 ) {

a2578f078a538115a06c6d2a2c383bdbfeeab140
Daniel Najdrowski <danielnajdrowski@gmail.com>
2024-02-25 15:38:20 +0100

    Connected AlertDialog to HomeScreen and handle logout logic from NavigationDrawer

--- app/src/main/java/pl/dnajdrowski/diaryapplication/navigation/NavGraph.kt
+++ app/src/main/java/pl/dnajdrowski/diaryapplication/navigation/NavGraph.kt
@@ -3,7 +3,11 @@ package pl.dnajdrowski.diaryapplication.navigation
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -13,10 +17,15 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.stevdzasan.messagebar.rememberMessageBarState
 import com.stevdzasan.onetap.rememberOneTapSignInState
+import io.realm.kotlin.mongodb.App
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import pl.dnajdrowski.diaryapplication.presentation.components.DisplayAlertDialog
 import pl.dnajdrowski.diaryapplication.presentation.screens.auth.AuthenticationScreen
 import pl.dnajdrowski.diaryapplication.presentation.screens.auth.AuthenticationViewModel
 import pl.dnajdrowski.diaryapplication.presentation.screens.home.HomeScreen
+import pl.dnajdrowski.diaryapplication.util.Constants.APP_ID
 import pl.dnajdrowski.diaryapplication.util.Constants.WRITE_SCREEN_ARGUMENT_KEY
 import java.lang.Exception
 
@@ -38,6 +47,10 @@ fun SetupNavGraph(
         homeRoute(
             navigateToWrite = {
                 navController.navigate(Screen.Write.route)
+            },
+            navigateToAuth = {
+                navController.popBackStack()
+                navController.navigate(Screen.Authentication.route)
             }
         )
         writeRoute()
@@ -88,20 +101,45 @@ fun NavGraphBuilder.authenticationRoute(
 }
 
 fun NavGraphBuilder.homeRoute(
-    navigateToWrite: () -> Unit
+    navigateToWrite: () -> Unit,
+    navigateToAuth: () -> Unit
 ) {
     composable(route = Screen.Home.route) {
         val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
         val scope = rememberCoroutineScope()
+        var singOutDialogOpened by remember { mutableStateOf(false) }
+
         HomeScreen(
             drawerState = drawerState,
-            onSignOutClicked = { },
+            onSignOutClicked = {
+                singOutDialogOpened = true
+            },
             onMenuClicked = {
                 scope.launch {
                     drawerState.open()
                 }
             },
             navigateToWrite = navigateToWrite
+        )
+
+        DisplayAlertDialog(
+            title = "Sign Out",
+            message = "Are you sure you want to Sign Out from your Google Accout?",
+            dialogOpened = singOutDialogOpened,
+            onDialogClosed = {
+                singOutDialogOpened = false
+            },
+            onYesClicked = {
+                scope.launch(Dispatchers.IO) {
+                    val user = App.Companion.create(APP_ID).currentUser
+                    user?.let {
+                        it.logOut()
+                        withContext(Dispatchers.Main) {
+                            navigateToAuth()
+                        }
+                    }
+                }
+            }
         )
     }
 }

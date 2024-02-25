f459db8285c8cde1294e8ed90bd0f8ba35744ca5
Daniel Najdrowski <danielnajdrowski@gmail.com>
2024-02-25 16:45:22 +0100

    Enabled core library desugaring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,9 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+        isCoreLibraryDesugaringEnabled = true
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }

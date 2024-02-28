3d864c22593efb6220a48ded1efbf0652a78455a
Daniel Najdrowski <danielnajdrowski@gmail.com>
2024-02-28 16:33:42 +0100

    Added hide keyboard logic onAddClicked in GalleryUploader

--- app/src/main/java/pl/dnajdrowski/diaryapplication/presentation/screens/write/WriteContent.kt
+++ app/src/main/java/pl/dnajdrowski/diaryapplication/presentation/screens/write/WriteContent.kt
@@ -172,7 +172,9 @@ fun WriteContent(
             Spacer(modifier = Modifier.height(12.dp))
             GalleryUploader(
                 galleryState = galleryState,
-                onAddClicked = { },
+                onAddClicked = {
+                    focusManager.clearFocus()
+                },
                 onImageSelect = onImageSelect,
                 onImageClicked = { }
             )
